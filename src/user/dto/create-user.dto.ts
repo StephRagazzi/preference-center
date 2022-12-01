a450b3aca41c9cca6e1e432ae8f73765ae4f87a7
Stephane Ragazzi <StephRagazzi@users.noreply.github.com>
2022-12-01 14:04:30 +0100

    Check if email address is valid

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,12 @@
+import { IsEmail } from "class-validator";
 import { IConsent, IConsents } from "../interfaces/consent.interface";
 import { IUser } from "../interfaces/user.interface";
 
 export class CreateUserDto implements IUser, IConsents {
     readonly id: string;
+
+    @IsEmail()
     readonly email: string;
+
     readonly consents: IConsent[];
 }
